c45a28aa6e7de707a337c7de187737953db297ed
Ikeh Leonard <leoikeh99@gmail.com>
2022-04-07 20:00:49 +0100

    Update database config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,10 @@
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
+  proxy: true,
+  app: {
+    keys: env.array("APP_KEYS", [
+      "DATABASE_URL",
+      "HEROKU_POSTGRESQL_MAROON_URL",
+    ]),
+  },
 });
